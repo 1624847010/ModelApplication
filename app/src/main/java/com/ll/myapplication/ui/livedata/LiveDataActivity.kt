2510b503b7708859913d590cc72a061ea8ce700f
ll <1624847010@qq.com>
2022-04-06 18:29:48 +0800

    Delegates

--- app/src/main/java/com/ll/myapplication/ui/livedata/LiveDataActivity.kt
+++ app/src/main/java/com/ll/myapplication/ui/livedata/LiveDataActivity.kt
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.blankj.utilcode.util.LogUtils
 import com.ll.myapplication.R
+import kotlinx.coroutines.delay
+import kotlin.properties.Delegates
 
 class LiveDataActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         @JvmStatic
         fun start(context: Context) {
             val starter = Intent(context, LiveDataActivity::class.java)
@@ -18,16 +21,52 @@ class LiveDataActivity : AppCompatActivity() {
         }
     }
 
-    private val model by viewModels<LiveDataViewModel> { LiveDataFactory(application , 1) }
+    private val model by viewModels<LiveDataViewModel> { LiveDataFactory(application, 1) }
+
+    //监听数值变动
+    private var a by Delegates.observable(0) { _, old, new ->
+        LogUtils.d("$old => $new")
+        onStateChangeListener?.onStateChanged(old,new)
+    }
+
+    //监听数值变动 并判断是否允许修改
+    private var b by Delegates.vetoable(0) { _, old, new ->
+        LogUtils.d("$old => $new")
+        onStateChangeListener?.onStateChanged(old,new)
+        old>new
+    }
+
+    private var onStateChangeListener: OnStateChangeListener? = null
+
+    interface OnStateChangeListener {
+        fun onStateChanged(old: Int, new: Int)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_live_data)
 
-        model.mediatorData.observe(this){
+        model.mediatorData.observe(this) {
             findViewById<TextView>(R.id.tv_1).text = it
         }
 
+        lifecycleScope.launchWhenCreated {
+            delay(1000)
+            ++a
+            --b
+            delay(1000)
+            ++a
+            --b
+            delay(1000)
+            ++a
+            --b
+        }
+
+        onStateChangeListener = object : OnStateChangeListener{
+            override fun onStateChanged(old: Int, new: Int) {
+                LogUtils.d("${Thread.currentThread()}  $old => $new")
+            }
+        }
         model.getMediator()
     }
 }
